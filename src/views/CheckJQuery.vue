1e8535de424674c6e38f44b9164fbc532dc6a91a
nikitavir1 <nikitavir1@gmail.com>
2022-04-26 18:51:36 +0300

    upd

--- src/views/CheckJQuery.vue
+++ src/views/CheckJQuery.vue
@@ -1,70 +1,47 @@
-<template>
-  <div class="jqery-test">
-    <h2>This is a heading</h2>
-
-    <p>This is a paragraph.</p>
-    <p>This is another paragraph.</p>
-
-    <button class="btn btn-success">Click me</button>
-
-    <button class="btn btn-danger">Click me 2</button>
-    <hr />
-    <p id="p1">This is a paragraph.</p>
-    <hr />
-    <div class="container">
-      <div class="row ">
-        <div
-          v-for="(item, index) in items"
-          :key="index"
-          class="col-2 text-center card mx-3 my-2"
-        >
-          <p class="h5">
-            {{
-              item.id == 1
-                ? "Мыло"
-                : item.id == 2
-                ? "Расчёска"
-                : item.id == 3
-                ? "Гель"
-                : ""
-            }}
-          </p>
-          <p class="text-muted">Артикул: {{ item.article }}</p>
-          <p>{{ item.price }} Р</p>
-          <div class="row">
-            <button :item-id="item.id" :item-artice="item.article" name="dec">
-              -
-            </button>
-            <input
-              :value="item.count"
-              :item-id="item.id"
-              :item-artice="item.article"
-              name="change-count"
-            />
-            <button :item-id="item.id" :item-artice="item.article" name="inc">
-              +
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <hr />
-  </div>
-</template>
-<script>
+<script setup>
 import jQuery from "jquery";
 const $ = jQuery;
 window.$ = $;
 
 $(document).ready(() => {
-  $("button").click((e) => {
-    let buttomInfo = { ...e.currentTarget.__vnode.props };
-    if (buttomInfo.name == "dec") {
-      console.log("уменьшить");
-    } else if (buttomInfo.name == "inc") {
+  $(document).on("click", "button", function() {
+    let buttonName = $(this).attr("name");
+    console.log(
+      $(this)
+        .closest(".catalog")
+        .data()
+    );
+    if (buttonName == "inc-count-catalog") {
       console.log("увеличить");
+    } else if (buttonName == "dec-count-catalog") {
+      console.log("уменьшить");
+    } else if (buttonName == "open-mini-cart") {
+      $("#mini-cart").show("slow");
+      $(this).hide("slow");
+    } else if (buttonName == "close-mini-cart") {
+      $("#mini-cart").hide("slow");
+      $("[name=open-mini-cart]").show();
     }
+
+    // $(this).fadeOut(1000);
+    return false;
   });
+  // $("button").click((e) => {
+  //   console.log(e);
+  //   console.log(e.attr(""));
+  //   let buttomInfo = { ...e.currentTarget.__vnode.props };
+  //   if (buttomInfo.name == "dec-count-catalog") {
+  //     console.log("уменьшить");
+  //   } else if (buttomInfo.name == "inc-count-catalog") {
+  //     console.log("увеличить");
+  //   } else if (buttomInfo.name == "open-mini-cart") {
+  //     console.log("asddasas");
+  //     $("#mini-cart").show("slow");
+  //   } else if (buttomInfo.name == "close-mini-cart") {
+  //     console.log("asddasas");
+  //     $("#mini-cart").hide("slow");
+  //   }
+  // });
 
   $("input").change((e) => {
     let buttomInfo = { ...e.currentTarget.__vnode.props };
@@ -86,7 +63,68 @@ $(document).ready(() => {
     console.log("ADD");
   });
 });
+</script>
 
+<template>
+  <div class="jqery-test">
+    <h2>This is a heading</h2>
+
+    <p>This is a paragraph.</p>
+    <p>This is another paragraph.</p>
+
+    <button class="btn btn-success">Click me</button>
+
+    <button class="btn btn-danger">Click me 2</button>
+    <hr />
+    <p id="p1">This is a paragraph.</p>
+    <hr />
+
+    <hr />
+    <button name="open-mini-cart">Открыть мини корзину</button>
+    <div id="mini-cart" style="display:none">
+      <button name="close-mini-cart">Закрыть корзину</button>
+      adsads
+    </div>
+    <hr />
+    <div class="container">
+      <div class="row ">
+        <div
+          v-for="(item, index) in items"
+          :key="index"
+          class="col-2 text-center card mx-3 my-2 catalog"
+          :data-id="item.id"
+          :data-article="item.article"
+        >
+          <p class="h5">
+            {{
+              item.id == 1
+                ? "Мыло"
+                : item.id == 2
+                ? "Расчёска"
+                : item.id == 3
+                ? "Гель"
+                : ""
+            }}
+          </p>
+          <p class="text-muted">Артикул: {{ item.article }}</p>
+          <p>{{ item.price }} Р</p>
+          <div class="row">
+            <button name="dec-count-catalog">
+              -
+            </button>
+            <input :value="item.count" name="change-count-catalog" />
+            <button name="inc-count-catalog">
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
 export default {
   name: "CheckJQuery",
   methods: {
